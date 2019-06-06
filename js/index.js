ddade4b8b60ae63793c1f949615206f438b5f943
Brian <lawmen218@yahoo.com>
2019-06-06 00:57:46 -0400

    completed!

--- js/index.js
+++ js/index.js
@@ -14,29 +14,73 @@ const siteContent = {
     "img-src": "img/header-img.png"
   },
   "main-content": {
-    "features-h4":"Features",
+    "features-h4": "Features",
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
+    "about-h4": "About",
     "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
+    "services-h4": "Services",
     "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
+    "product-h4": "Product",
     "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
+    "vision-h4": "Vision",
     "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
   "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+    "contact-h4": "Contact",
+    "address": "123 Way 456 Street Somewhere, USA",
+    "phone": "1 (888) 888-8888",
+    "email": "sales@greatidea.io",
   },
   "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
+    "copyright": "Copyright Great Idea! 2018"
   },
 };
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const h1Content = document.querySelector("h1");
+// console.log(h1Content);
+h1Content.innerHTML = "DOM <br> is <br> Awesome";
+
+
+const aTag = document.querySelectorAll("a");
+aTag[0].innerText = "Services";
+aTag[1].innerText = "Product";
+aTag[2].innerText = "Vision";
+aTag[3].innerText = "Features";
+aTag[4].innerText = "About";
+aTag[5].innerText = "Contact";
+
+const btnContent = document.querySelector("button");
+btnContent.textContent = "Get Started";
+
+const hImage = document.querySelector("#cta-img");
+hImage.setAttribute("src", siteContent["cta"]["img-src"])
+
+const headerText = document.querySelectorAll("h4");
+// console.log(headerText);
+headerText[0].textContent = "Features";
+headerText[1].innerText = "About";
+headerText[2].textContent = "Services";
+headerText[3].textContent = "Product";
+headerText[4].textContent = "Vision";
+headerText[5].textContent = "Contact";
+
+const mcText = document.querySelectorAll("p");
+// console.log(mcText);
+mcText[0].innerText = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis";
+mcText[1].innerText = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis";
+mcText[2].innerText = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mcText[3].innerText = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mcText[4].innerText = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mcText[5].innerHTML = "123 Way 456 Street <br> Somewhere, USA";
+mcText[6].innerText = "1 (888) 888-8888";
+mcText[7].innerText = "sales@greatidea.io";
+mcText[8].innerText = "Copyright Great Idea! 2018";
+
+
+const mainImage = document.querySelector("#middle-img");
+mainImage.setAttribute("src", siteContent["main-content"]["middle-img-src"]);
